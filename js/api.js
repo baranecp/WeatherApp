42b6d28dddf2e8fd65f23c5b4e27aa9f745f3f6c
baranecp <baranec.dev@gmail.com>
2025-08-07 17:04:21 +0200

    Add some styling and icons

--- js/api.js
+++ js/api.js
@@ -2,23 +2,23 @@ import { render_left_side } from "./weatherUI";
 
 document.addEventListener("DOMContentLoaded", function () {
   const apiKey = "72850a547f4f40d1bd6111420250708";
-  let city = "London";
+  let city = "Bratislava";
   const search_btn = document.querySelector(".search_div");
   let city_input = document.querySelector(".search input");
 
-  city_input.addEventListener("input", function () {
-    if (city_input.value.trim() !== "") {
-      search_btn.classList.remove("disabled");
-    } else {
-      search_btn.classList.add("disabled");
-    }
-  });
+  // city_input.addEventListener("input", function () {
+  //   if (city_input.value.trim() !== "") {
+  //     search_btn.classList.remove("disabled");
+  //   } else {
+  //     search_btn.classList.add("disabled");
+  //   }
+  // });
 
-  search_btn.addEventListener("click", () => {
-    if (city_input.value.trim() !== "") {
-      get_weather_data(city_input.value, apiKey);
-    }
-  });
+  // search_btn.addEventListener("click", () => {
+  //   if (city_input.value.trim() !== "") {
+  //     get_weather_data(city_input.value, apiKey);
+  //   }
+  // });
 
   async function get_weather_data(city, apiKey) {
     try {
--- js/weatherUI.js
+++ js/weatherUI.js
@@ -1,18 +1,19 @@
 import "../src/style.css";
 import { dateFormatter } from "./utils/dateFormatter";
+import sunny from "../src/img/sunny.svg";
 
 export function render_left_side(current, location, condition) {
-  const section = document.querySelector("section.currnet");
+  const section = document.querySelector("section.current");
   const currentTemp = document.querySelector(".current__temp");
   const currentCity = document.querySelector(".current__city");
   const currentDayTime = document.querySelector(".current__daytime");
 
-  currentTemp.textContent = `${current.temp_c}°`;
+  currentTemp.textContent = `${Math.round(current.temp_c)}°`;
   currentCity.textContent = `${location.name}`;
   currentDayTime.textContent = `${dateFormatter(location.localtime)}`;
 
   const img = document.createElement("img");
-  img.src = condition.icon;
+  if (condition.text === "Sunny") img.src = sunny;
   img.alt = condition.text;
   const span = section.querySelector("span");
   section.insertBefore(img, span.nextSibling);
